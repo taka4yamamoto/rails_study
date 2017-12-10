b00ea4d747eff714eee52bbfe94fb3c16d119b18
taka4yamamoto <tksymmt.ymtk@gmail.com>
2017-12-10 15:27:04 +0900

    add about and contact pages

--- spec/controllers/static_pages_controller_spec.rb
+++ spec/controllers/static_pages_controller_spec.rb
@@ -1,11 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe StaticPagesController, type: :controller do
+  render_views
+
+  let(:base_title) { 'Ruby on Rails Tutorial Sample App' }
 
   describe "GET #home" do
     it "returns http success" do
       get :home
       expect(response).to have_http_status(:success)
+      assert_select "title", "Home | #{base_title}"
     end
   end
 
@@ -13,7 +17,23 @@ RSpec.describe StaticPagesController, type: :controller do
     it "returns http success" do
       get :help
       expect(response).to have_http_status(:success)
+      assert_select "title", "Help | #{base_title}"
     end
   end
 
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+      assert_select "title", "About | #{base_title}"
+    end
+  end
+
+  describe "GET #contact" do
+    it "returns http success" do
+      get :contact
+      expect(response).to have_http_status(:success)
+      assert_select "title", "Contact | #{base_title}"
+    end
+  end
 end
